b69e6661785fbe4b291b04779d1b24a3de3c38a9
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-04-17 16:24:19 +0700

    Hasil Output

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -75,3 +75,11 @@ void mergeSort(int low, int high){
         arr[x] = B[x];
     }
 }
+
+void output(){
+    cout << "\nData Setelah Diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
