ca50fb5574a7042a161b3305cb45fa4cfb8e8c87
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-04-17 16:30:47 +0700

    Memperbagus Komentar 2

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -30,16 +30,16 @@ void input(){
 
 //create function mergesort
 void mergeSort(int low, int high){
-    if (low >= high){ //step 1
-        return; //step 1.a
+    if (low >= high){   //step 1
+        return;         //step 1.a
     } 
 
     int mid = (low + high) / 2; //step 2
 
     //step 3
     //fungsi rekursi - memanggil diri sendiri
-    mergeSort(low, mid); //step 3.a
-    mergeSort(mid + 1, high); //step 3.b
+    mergeSort(low, mid);        //step 3.a
+    mergeSort(mid + 1, high);   //step 3.b
 
     //step 4
     int i = low;        //step 4.a
@@ -65,7 +65,7 @@ void mergeSort(int low, int high){
     }
 
     while (i <= mid){   //step 4.f
-        B[k] = arr[i]; //step 4.f.i
+        B[k] = arr[i];  //step 4.f.i
         i++;            //step 4.f.ii
         k++;            //step 4.f.iii
     }
