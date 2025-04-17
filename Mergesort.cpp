e77983a47c12656e8dc7f46677113dc8444296f6
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-04-17 16:17:22 +0700

    Memberikan Batasan untuk Array Maksimal 20

--- Mergesort.cpp
+++ Mergesort.cpp
@@ -7,5 +7,23 @@ int arr[20], B[20];
 int n;
 
 void input(){
+    while (true){
+        cout << "Masukkan Panjang Element Array : ";
+        cin >> n;
 
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nMaksimal Panjang Array Adalah 20" << endl;
+        }
+    }
+    cout << "\n--------------------------" << endl;
+    cout << "\nInputkan Isi Element array" << endl;
+    cout << "\n--------------------------" << endl;
+
+    for (int i = 0; i < n; i++){
+        cout << "Array Index Ke- " << i << " : ";
+        cin >> arr[i];
+    }
 }
